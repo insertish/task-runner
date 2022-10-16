c3e52d0472ec2980261afe103f1f887585ff234b
Paul Makles <paulmakles@gmail.com>
2022-10-16 10:28:27 +0100

    feat: add conditional triggers

--- src/web.mjs
+++ src/web.mjs
@@ -5,6 +5,8 @@ import { Triggers } from './config/triggers.mjs'
 const app = express()
 const port = 10680
 
+app.use(express.json());
+
 app.get('/', (_, res) => {
   res.send('Hello from Task Runner!')
 })
@@ -15,8 +17,31 @@ app.listen(port, () => {
 
 for (const name of Object.keys(Triggers.webhook)) {
     const webhook = Triggers.webhook[name];
-    app[webhook.method](`/api/v1/trigger/webhook/${name}`, async (_, res) => {
-        await runFlow(webhook.flow);
-        res.sendStatus(204);
+    app[webhook.method](`/api/v1/trigger/webhook/${name}`, async (req, res) => {
+      if (webhook.if) {
+        for (const condition of webhook.if) {
+          if (condition.type === 'body') {
+            let ref = req.body;
+            const path = condition.key.split('.');
+            while (ref && path.length > 0) {
+              ref = ref[path.shift()];
+            }
+
+            if (path.length > 0) {
+              throw "Did not consume all tokens!";
+            }
+
+            if (ref !== condition.value) {
+              res.sendStatus(204);
+              console.info("Ignoring request as condition did not match!");
+              return;
+            }
+          }
+        }
+      }
+
+
+      await runFlow(webhook.flow);
+      res.sendStatus(204);
     });
 }
